99216170f7d30f3fbb9575a678c868a8d1c47087
Povo43 <140213985+Povo43@users.noreply.github.com>
2024-04-08 12:26:26 +0900

    aaaaaaaaa

---
+++ login_signin/New.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="ja">
+ <head>
+ <meta charset="utf-8">
+ <title>ログイン - BookTower</title>
+ <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ <link rel="icon" href="../images/logos/mima-5.jpeg"/>
+ <link rel="stylesheet" href="./login_signin.css">
+ <link rel="stylesheet" href="./signin.css">
+ </head>
+ <body>
+    <header>
+			<a href="../../index.html" class="header-logo"><img src="../images/logos/mima-5.jpeg" alt="logo" class="logo"></a>
+	</header>
+
+ <div class="forms">
+	<div class="form_bg">
+		<div id="login_title">アカウントの作成</div>
+		<form action="" method="POST">
+	   	    <input class="input" id="id" name="username" type="text" placeholder="ユーザー名">
+    	    <input class="input" id="email" name="password" type="text" placeholder="メールアドレス">
+    	    <p class="form-p">登録できるのはoutlook.com、yahoo.co.jp,gmail.comのみです</p>
+            <input class="input" id="signin-pass" name="password" type="text" placeholder="パスワード">
+			<p class="form-p"></p>
+    	    <button class="form-button" name="signin" type="submit">作成</button>
+    	</form>
+	</div>
+</div>
+
+	<footer>
+		<div class="footer">
+			<p><small>Copyright &copy; 2024 Povo Tako All Rights Reserved.</small></p>
+			<a href="" class="footer-a">プライバシーポリシー</a>
+		</div>
+ 	</footer>
+ </body>
+</html>
---
+++ login_signin/login.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="ja">
+ <head>
+ <meta charset="utf-8">
+ <title>ログイン - BookTower</title>
+ <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ <link rel="icon" href="../images/logos/mima-5.jpeg"/>
+ <link rel="stylesheet" href="./login_signin.css">
+ <link rel="stylesheet" href="./login.css">
+ </head>
+ <body>
+    <header>
+			<a href="../../index.html" class="header-logo"><img src="../images/logos/mima-5.jpeg" alt="logo" class="logo"></a>
+	</header>
+
+ <div class="forms">
+	<div class="form_bg">
+		<div id="login_title">ログイン</div>
+		<form action="" method="POST">
+	   	    <input id="signin-id" name="username" type="text" placeholder="ユーザー名 または メールアドレス">
+    	    <input id="signin-pass" name="password" type="text" placeholder="パスワード">
+			<p class="form-p">アカウントがありませんか?<a href="New.php">作成</a>してみましょう!</p>
+    	    <button class="form-button" name="signin" type="submit">ログインする</button>
+    	</form>
+	</div>
+</div>
+
+	<footer>
+		<div class="footer">
+			<p><small>Copyright &copy; 2024 Povo Tako All Rights Reserved.</small></p>
+			<a href="" class="footer-a">プライバシーポリシー</a>
+		</div>
+ 	</footer>
+ </body>
+</html>
